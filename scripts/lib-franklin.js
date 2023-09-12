e1f3528689200199caaa2e0d2731e94f8309d52b
Johnny Clark <jclark@r2integrated.com>
2023-09-12 11:00:34 -0600

    Added a johnniary class to buttons

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -601,7 +601,7 @@ export function decorateButtons(element) {
         }
         if (up.childNodes.length === 1 && up.tagName === 'STRONG'
           && twoup.childNodes.length === 1 && twoup.tagName === 'P') {
-          a.className = 'button primary';
+          a.className = 'button johnniary';
           twoup.classList.add('button-container');
         }
         if (up.childNodes.length === 1 && up.tagName === 'EM'
